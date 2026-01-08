df00bcda5dc84688c436fa7c06ea98c5b8949ea2
hhzzlll <2022217577@mail.hfut.edu.cn>
2026-01-08 23:04:48 +0800

    finish

--- miniprogram/config/api.ts
+++
@@ -1,9 +0,0 @@
-// API 配置文件
-export const API_CONFIG = {
-  // 豆包 API Key
-  DOUBAO_API_KEY: '46795c0b-7f62-40af-8934-931d40d61066',
-  // 豆包文生图接口地址
-  DOUBAO_TEXT2IMG_URL: 'https://ark.cn-beijing.volces.com/api/v3',
-  // 超时设置
-  TIMEOUT: 60000,
-}
